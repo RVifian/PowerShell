cf6b13d547061e5183c9ca9dd81b26a06c13b0e9
Robin Vifian <robin.vifian@gmail.com>
2023-07-26 16:45:38 +0200

    beautify

--- Get all VMs with specified OS.ps1
+++ Get all VMs with specified OS.ps1
@@ -1,11 +1,19 @@
+# Script to Get VM with specified OS
+# v1.0 26.07.2023
+
+$server = Read-Host -Prompt "What Server would you like to connect to?"
+
 # Connect to a VMware vSphere server
-Connect-VIServer
+Connect-VIServer $server
+
+# Get search criteria
+$SearchCriteria = Read-Host -Prompt "What are you searching for?"
 
 # Get a list of virtual machines
 Get-VM |
 
 # Filter the virtual machines whose Guest Fullname (Operating System) contains "2022"
-where{$_.ExtensionData.Config.GuestFullname -match "2022"} |
+where{$_.ExtensionData.Config.GuestFullname -match "$SearchCriteria"} |
 
 # Select the following properties for each virtual machine:
 Select Name,
