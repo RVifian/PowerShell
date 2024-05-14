13f0b18dc89c7119592a0ce0378c6443904b8f55
Robin Vifian <robin.vifian@gmail.com>
2024-05-14 11:23:59 +0200

    updated code

--- PRTG.ps1
+++ PRTG.ps1
@@ -1,4 +1,3 @@
-
 # Script to Connect to PRTG and migrate Trend Micro
 # v1.0 13.05.2024
 
@@ -9,10 +8,29 @@
 #3. Import-Module PrtgAPI
 #4. Connect-PrtgServer
 
-#List all devices (Get-Device)
+# Check if the PrtgAPI module is installed, and if not, install it.
+if (Get-Module -ListAvailable -Name PrtgAPI) {
+} 
+else {
+    Install-Module -Name PrtgAPI
+}
+
+# Set the execution policy to unrestricted to allow the script to run.
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+# Import the PrtgAPI module.
+Import-Module PrtgAPI
+
+#List all devices 
+Get-Device
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
+# Prompt the user to enter the username for which they want to retrieve session information.
+$endpoint = Read-Host -Prompt "For which endpoint ID would you like to add sensors?"
 
 #Device to install sensors
-$device = Get-Device -Id 3629
+$device = Get-Device -Id $endpoint
 
 #List all Trend Micro devices
 $device | get-sensor | Where-Object {$_.Name -like "*Trend*"}
@@ -20,14 +38,25 @@ $device | get-sensor | Where-Object {$_.Name -like "*Trend*"}
 #List all TM Apex devices
 $device | get-sensor | Where-Object {$_.Name -like "*Apex One*"}
 
+Write-Host -NoNewLine 'Press any key to remove old sensors';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
 #Remove Old TM Apex Sensors
 $device | get-sensor | Where-Object {$_.Name -like "*Apex One*"} | Remove-Object
 
 #Confirm they are removed
 $device | get-sensor | Where-Object {$_.Name -like "*Apex One*"}
 
+
+
 #Get all current Trend Micro Services on the machine
 $params = $device | Get-SensorTarget wmiservice "Trend Micro*" -Params
+
+#Display found services
+$params
+
+Write-Host -NoNewLine 'Press any key to add new sensors';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
 #Add Sensors to device
 $device | Add-Sensor $params
