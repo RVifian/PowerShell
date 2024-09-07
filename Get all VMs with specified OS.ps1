a0d135601b1b973738548c2acd9abd7957a81d20
Robin Vifian <robin.vifian@gmail.com>
2024-09-07 23:30:20 +0200

    VMware.PowerCLI recognition

--- Get all VMs with specified OS.ps1
+++ Get all VMs with specified OS.ps1
@@ -1,6 +1,16 @@
 # Script to Get VM with specified OS
 # v1.0 26.07.2023
 
+# Check if the VMware PowerCLI module is installed, and if not, install it.
+if (Get-Module -ListAvailable -Name VMware.PowerCLI) {
+} 
+else {
+    Install-Module -Name VMware.PowerCLI
+}
+
+# Import the VMware PowerCLI module.
+Import-Module VMware.PowerCLI
+
 $server = Read-Host -Prompt "What Server would you like to connect to?"
 
 # Connect to a VMware vSphere server
@@ -13,10 +23,10 @@ $SearchCriteria = Read-Host -Prompt "What are you searching for?"
 Get-VM |
 
 # Filter the virtual machines whose Guest Fullname (Operating System) contains $SearchCriteria
-where{$_.ExtensionData.Config.GuestFullname -match "$SearchCriteria"} |
+Where-Object{$_.ExtensionData.Config.GuestFullname -match "$SearchCriteria"} |
 
 # Select the following properties for each virtual machine:
-Select Name,
+Select-Object Name,
 
     # Custom calculated property: Configured OS (Guest Fullname from virtual machine's configuration)
     @{N="Configured OS";E={$_.ExtensionData.Config.GuestFullname}},
